95e6b35d53036f830cab65e76e0bc7def0daa302
Simon Pino-Buisson <98675268+spynob@users.noreply.github.com>
2023-11-04 11:31:36 -0400

    Initial commit

---
+++ src/main/java/com/quantumslots/physhacks/PhyshacksApplication.java
@@ -0,0 +1,13 @@
+package com.quantumslots.physhacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhyshacksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhyshacksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quantumslots/physhacks/PhyshacksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantumslots.physhacks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhyshacksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
