6b6a0b5a4b82d99345a1279ff01e284655702b15
Epoch <vincivin2608@gmail.com>
2023-11-05 09:55:36 -0500

    PLOT SCALING CATASTROPHIC FAILURE

--- src/main/java/com/quantumslots/physhacks/service/gui/GUITest.java
+++ src/main/java/com/quantumslots/physhacks/service/gui/GUITest.java
@@ -1,107 +1,88 @@
 package com.quantumslots.physhacks.service.gui;
 
+import com.quantumslots.physhacks.controllers.HomeController;
+import com.quantumslots.physhacks.service.TimeService;
 import com.quantumslots.physhacks.service.gui.PlotService;
 import javafx.application.Application;
 import javafx.embed.swing.SwingNode;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.scene.transform.Scale;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.shape.Rectangle;
 
 import org.jfree.chart.ChartPanel;
 
-import javax.swing.*;
-import java.awt.*;
-
-import static javafx.scene.paint.Color.BLUE;
-import static javafx.scene.paint.Color.RED;
-
 public class GUITest extends Application {
+    private HomeController homeController;
+    private TimeService timeService;
+    public GUITest() {
+        // No-argument constructor
+    }
+
+    public GUITest(HomeController homeController, TimeService timeService) {
+        this.homeController = homeController;
+        if (timeService == null) {
+            throw new IllegalArgumentException("timeService is null");
+        }
+        this.timeService = timeService;
+    }
+
+
     @Override
     public void start(Stage stage) {
-
-        // Create a SwingNode
-        SwingNode swingNode = new SwingNode();
-
         // Create a new JFreeChart using PlotService
-        PlotService plotService = new PlotService();
-        SwingUtilities.invokeLater(() -> {
-            //plotService.pack();
-            plotService.setVisible(true);
-        });
-
-        // Set the content of the SwingNode to the ApplicationFrame
-        swingNode.setContent(plotService);
-
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root, 800, 600, RED);
-
-        plotService.setSize(1000, 100);
+        PlotService plotService = new PlotService("");
         ChartPanel chartPanel = new ChartPanel(plotService.getChart());
-        //StackPane root = new StackPane();
-        root.getChildren().add(swingNode);
-        Rectangle rectangle = new Rectangle(100, 100, BLUE);
-        root.getChildren().add(rectangle);
+
+        // Set the fixed dimensions for the chart
+        chartPanel.setPreferredSize(new java.awt.Dimension(550, 400)); // You can adjust the dimensions as needed
+
+        // Create a SwingNode to embed the JFreeChart
+        SwingNode chartNode = new SwingNode();
+        chartNode.setContent(chartPanel);
+
+        // Create a StackPane layout to center the chart on the left
+        StackPane leftPane = new StackPane(chartNode);
+
+        // Create a button and add an action to call the measure() method in HomeController
+        Button measureButton = new Button("Make Measurement");
+        measureButton.setOnAction(e -> homeController.measure(timeService.getCurrentTimeMillis() / 1000.0));
 
 
+        // Create a VBox to organize the button and text
+        VBox rightPane = new VBox(measureButton);
+        rightPane.setSpacing(10); // Add spacing between button and text
 
+        // Create an HBox to hold both the chart and the right components
+        HBox root = new HBox(leftPane, rightPane);
 
+        // Add padding to separate the chart from other components
+        root.setPadding(new Insets(10));
 
-        //JFrame
-        //JFrame myJFrame = new JFrame("Normal JFrame");
+        // Create the scene
+        Scene scene = new Scene(root, 1280, 600);
 
-
-
-        //Graph into JFrame
-
-        // Put Graph into Node
-        //SwingNode graphNode = new SwingNode();
-        //graphNode.setContent(chartPanel);
-
-        //Constraining Node into StackPane
-        /* StackPane wrappedNode = new StackPane(graphNode);
-
-        wrappedNode.setPrefWidth(150); // Set the desired width
-        wrappedNode.setPrefHeight(75); // Set the desired height */
-
-// Create the JavaFX scene and stage
-        //StackPane root = new StackPane(graphNode);
-
-        // Create a scene and set it to the root node
-
-
-        // Create a rectangle (your content)
-        //Rectangle rectangle = new Rectangle(100, 100, BLUE);
-        //root.getChildren().add(graphNode);
-        // Add the rectangle to the root
-
-
-        //SwingNode swingNode = new SwingNode();
-
-        // Create a JPanel to host your Swing UI
-        JPanel panel = new JPanel();
-        panel.add(new JButton("Spin!"));
-
-        // Set the content of the SwingNode to the JPanel
-        swingNode.setContent(panel);
-
-/////////////////////////////////////////////////////////////
+        // Set the title
+        stage.setTitle("Schrödinger's Slot Machine");
 
         // Set the scene to the stage
         stage.setScene(scene);
-
-        // Set the stage title
-        stage.setTitle("SwingNode Example");
 
         // Show the stage
         stage.show();
     }
 
-
     public static void main(String[] args) {
-        launch();
+        TimeService timeService = TimeService.getInstance(); // Obtain the TimeService instance
+
+        HomeController homeController = new HomeController(); // Initialize HomeController
+        GUITest guiTest = new GUITest(homeController, timeService); // Initialize GUITest with the TimeService instance
+
+        // Proceed with launching the JavaFX application
+        launch(args);
     }
+
 }
