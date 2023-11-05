a0e187b82b00917a326462e0f79bdf4cbaa9f3b3
simon <simon515lavoie@gmail.com>
2023-11-05 02:18:57 -0500

    Embedded animated plots into scene in GUI.

---
+++ src/main/java/com/quantumslots/physhacks/service/gui/GUI.java
@@ -0,0 +1,35 @@
+package com.quantumslots.physhacks.service.gui;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+    @Override
+    public void start(Stage stage) {
+        // Create a Text node with "Hello, World" text
+        Text text = new Text("Hello, World");
+
+        // Create a StackPane layout to center the text
+        StackPane root = new StackPane(text);
+
+        // Create the scene
+        Scene scene = new Scene(root, 300, 200);
+
+        // Set the title
+        stage.setTitle("Hello World Applet");
+
+        // Set the scene to the stage
+        stage.setScene(scene);
+
+        // Show the stage
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
+
--- src/main/java/com/quantumslots/physhacks/service/gui/PlotService.java
+++ src/main/java/com/quantumslots/physhacks/service/gui/PlotService.java
@@ -1,5 +1,7 @@
 package com.quantumslots.physhacks.service.gui;
 
+import com.quantumslots.physhacks.model.potentials.PotentialFunction;
+
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
@@ -23,93 +25,81 @@ public class PlotService extends ApplicationFrame {
     private JFreeChart chart;
     private double timer = 0;
     private final float animationInterval = 1000 / 20; // Adjust the animation speed as needed
+            public PlotService(String title) {
+            super(title);
 
-    public PlotService(String title) {
-        super(title);
+            series1 = new XYSeries("Function 1");
+            series2 = new XYSeries("Function 2");
+            dataset = new XYSeriesCollection();
+            dataset.addSeries(series1);
+            dataset.addSeries(series2);
 
-        series1 = new XYSeries("Function 1");
-        series2 = new XYSeries("Function 2");
-        dataset = new XYSeriesCollection();
-        dataset.addSeries(series1);
-        dataset.addSeries(series2);
+            chart = ChartFactory.createXYLineChart(
+                    "",
+                    "",
+                    "",
+                    dataset,
+                    PlotOrientation.VERTICAL,
+                    false, false, false
+            );
 
-        chart = ChartFactory.createXYLineChart(
-                "",
-                "",
-                "",
-                dataset,
-                PlotOrientation.VERTICAL,
-                false, false, false
-        );
+            chart.setBackgroundPaint(Color.BLACK); // Set the background color to black
 
-        chart.setBackgroundPaint(Color.BLACK); // Set the background color to black
+            // Customize the plot's gridlines (teal color)
+            XYPlot plot = chart.getXYPlot();
+            plot.setDomainGridlinePaint(Color.CYAN);
+            plot.setRangeGridlinePaint(Color.CYAN);
 
-        // Customize the plot's gridlines (teal color)
-        XYPlot plot = chart.getXYPlot();
-        plot.setDomainGridlinePaint(Color.CYAN);
-        plot.setRangeGridlinePaint(Color.CYAN);
+            // Remove numbers on the axes
+            NumberAxis xAxis = (NumberAxis) plot.getDomainAxis();
+            xAxis.setTickLabelsVisible(false);
 
-        // Remove numbers on the axes
-        NumberAxis xAxis = (NumberAxis) plot.getDomainAxis();
-        xAxis.setTickLabelsVisible(false);
+            NumberAxis yAxis = (NumberAxis) plot.getRangeAxis();
+            yAxis.setTickLabelsVisible(false);
 
-        NumberAxis yAxis = (NumberAxis) plot.getRangeAxis();
-        yAxis.setTickLabelsVisible(false);
+            // Set the plot area's background to black
+            plot.setBackgroundPaint(Color.BLACK);
 
-        // Set the plot area's background to black
-        plot.setBackgroundPaint(Color.BLACK);
+            // Set fixed axis bounds to prevent zooming
+            chart.getXYPlot().getDomainAxis().setRange(-1.0, 1.0);
+            chart.getXYPlot().getRangeAxis().setRange(-2.0, 2.0);
 
-        // Set fixed axis bounds to prevent zooming
-        chart.getXYPlot().getDomainAxis().setRange(-1.0, 1.0);
-        chart.getXYPlot().getRangeAxis().setRange(-2.0, 2.0);
-
-        ChartPanel chartPanel = new ChartPanel(chart);
-        chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
-        setContentPane(chartPanel);
-
-        // Create a Timer to update the graph at a specific interval
-        Timer graphTimer = new Timer((int) animationInterval, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+            // Create a Timer to update the graph at a specific interval
+            Timer graphTimer = new Timer((int) animationInterval, e -> {
                 timer += animationInterval / 1000; // Convert to seconds
                 updateGraph(timer);
+            });
+            graphTimer.start();
+        }
+
+        // Method to retrieve the JFreeChart instance
+        public JFreeChart getChart() {
+            return chart;
+        }
+
+        public void updateGraph(double t) {
+            series1.clear();
+            series2.clear();
+
+            int numPoints = 200; // Number of points to plot
+
+            for (int i = 0; i < numPoints; i++) {
+                double x = -1.0 + (2.0 / (numPoints - 1)) * i;
+                double amplitude1 = calculateFunction1(x, t);
+                double amplitude2 = calculateFunction2(x, t);
+
+                series1.add(x, amplitude1);
+                series2.add(x, amplitude2);
             }
-        });
-        graphTimer.start();
-    }
+        }
 
-    public void updateGraph(double t) {
-        series1.clear();
-        series2.clear();
+        private double calculateFunction1(double x, double t) {
+            // Replace this with your first function calculation
+            return Math.sin(2.0 * Math.PI * x) * Math.cos(2 * Math.PI * t);
+        }
 
-        int numPoints = 200; // Number of points to plot
-
-        for (int i = 0; i < numPoints; i++) {
-            double x = -1.0 + (2.0 / (numPoints - 1)) * i;
-            double amplitude1 = calculateFunction1(x, t);
-            double amplitude2 = calculateFunction2(x, t);
-
-            series1.add(x, amplitude1);
-            series2.add(x, amplitude2);
+        private double calculateFunction2(double x, double t) {
+            // Replace this with your second function calculation
+            return Math.cos(2.0 * Math.PI * x) * Math.sin(2 * Math.PI * t);
         }
     }
-
-    private double calculateFunction1(double x, double t) {
-        // Replace this with your first function calculation
-        return Math.sin(2.0 * Math.PI * x) * Math.cos(2 * Math.PI * t);
-    }
-
-    private double calculateFunction2(double x, double t) {
-        // Replace this with your second function calculation
-        return Math.cos(2.0 * Math.PI * x) * Math.sin(2 * Math.PI * t);
-    }
-
-    public static void main(String[] args) {
-        PlotService plotService = new PlotService("Animated Functions Example");
-        plotService.pack();
-        UIUtils RefineryUtilities = null;
-        RefineryUtilities.centerFrameOnScreen(plotService);
-        plotService.setVisible(true);
-    }
-}
-
