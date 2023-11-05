b823e9f337ccf808429dd821bd45b32d4aee4432
simon <simon515lavoie@gmail.com>
2023-11-04 22:38:30 -0400

    Live plotting implemented with a few aesthetic choices. Code outputs a window to plot. Need to decide how we integrate this to the app.

--- src/main/java/com/quantumslots/physhacks/service/gui/PlotService.java
+++ src/main/java/com/quantumslots/physhacks/service/gui/PlotService.java
@@ -3,19 +3,22 @@ package com.quantumslots.physhacks.service.gui;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.ui.ApplicationFrame;
 import org.jfree.chart.ui.UIUtils;
-
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class PlotService extends ApplicationFrame {
-    private XYSeries series;
+    private XYSeries series1;
+    private XYSeries series2;
     private XYSeriesCollection dataset;
     private JFreeChart chart;
     private double timer = 0;
@@ -24,16 +27,41 @@ public class PlotService extends ApplicationFrame {
     public PlotService(String title) {
         super(title);
 
-        series = new XYSeries("Wavefunction");
-        dataset = new XYSeriesCollection(series);
+        series1 = new XYSeries("Function 1");
+        series2 = new XYSeries("Function 2");
+        dataset = new XYSeriesCollection();
+        dataset.addSeries(series1);
+        dataset.addSeries(series2);
+
         chart = ChartFactory.createXYLineChart(
-                "Animated Wavefunction Graph",
-                "X",
-                "Amplitude",
+                "",
+                "",
+                "",
                 dataset,
                 PlotOrientation.VERTICAL,
-                true, true, false
+                false, false, false
         );
+
+        chart.setBackgroundPaint(Color.BLACK); // Set the background color to black
+
+        // Customize the plot's gridlines (teal color)
+        XYPlot plot = chart.getXYPlot();
+        plot.setDomainGridlinePaint(Color.CYAN);
+        plot.setRangeGridlinePaint(Color.CYAN);
+
+        // Remove numbers on the axes
+        NumberAxis xAxis = (NumberAxis) plot.getDomainAxis();
+        xAxis.setTickLabelsVisible(false);
+
+        NumberAxis yAxis = (NumberAxis) plot.getRangeAxis();
+        yAxis.setTickLabelsVisible(false);
+
+        // Set the plot area's background to black
+        plot.setBackgroundPaint(Color.BLACK);
+
+        // Set fixed axis bounds to prevent zooming
+        chart.getXYPlot().getDomainAxis().setRange(-1.0, 1.0);
+        chart.getXYPlot().getRangeAxis().setRange(-2.0, 2.0);
 
         ChartPanel chartPanel = new ChartPanel(chart);
         chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
@@ -51,28 +79,37 @@ public class PlotService extends ApplicationFrame {
     }
 
     public void updateGraph(double t) {
-        series.clear();
+        series1.clear();
+        series2.clear();
 
         int numPoints = 200; // Number of points to plot
 
         for (int i = 0; i < numPoints; i++) {
             double x = -1.0 + (2.0 / (numPoints - 1)) * i;
-            double amplitude = calculateWavefunction(x, t);
+            double amplitude1 = calculateFunction1(x, t);
+            double amplitude2 = calculateFunction2(x, t);
 
-            series.add(x, amplitude);
+            series1.add(x, amplitude1);
+            series2.add(x, amplitude2);
         }
     }
 
-    private double calculateWavefunction(double x, double t) {
-        // Replace this with your wavefunction calculation
+    private double calculateFunction1(double x, double t) {
+        // Replace this with your first function calculation
         return Math.sin(2.0 * Math.PI * x) * Math.cos(2 * Math.PI * t);
     }
 
+    private double calculateFunction2(double x, double t) {
+        // Replace this with your second function calculation
+        return Math.cos(2.0 * Math.PI * x) * Math.sin(2 * Math.PI * t);
+    }
+
     public static void main(String[] args) {
-        PlotService plotService = new PlotService("Animated Graph Example");
+        PlotService plotService = new PlotService("Animated Functions Example");
         plotService.pack();
         UIUtils RefineryUtilities = null;
         RefineryUtilities.centerFrameOnScreen(plotService);
         plotService.setVisible(true);
     }
 }
+
